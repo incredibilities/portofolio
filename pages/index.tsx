1533ef138610149bbaa46c66345e8f05a4cd79f8
vawnaire <63461297+vawnair@users.noreply.github.com>
2023-01-13 20:46:47 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function HomePage() {
 
 
 	return (
-		<Layout.Default seo={{ title: 'IUSTIN - CACACIOS' }}>
+		<Layout.Default seo={{ title: 'IUSTIN - m6 mafiA MEGHEKAPPA 888' }}>
 			{isBirthday && <Event event={EventType.BIRTHDAY} />}
 			<div className="min-h-screen flex items-center justify-center py-12">
 				<div className="max-w-md sm:max-w-lg md:sm:max-w-2xl lg:sm:max-w-3xl w-full space-y-8 text-center">
